90b046fc060d82cb79f60b2f6b8d4910460e09f3
HADI <HADI@DESKTOP-QF85E1O>
2021-12-20 20:15:52 +0200

    Add project files.

---
+++ ticTac/Board.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ticTac
+{
+	class Board
+	{
+		/// <summary>
+		/// Tic Tac Toe Gameboard states
+		/// </summary>
+		public string[,] GameBoard = new string[,]
+		{
+			{"1", "2", "3"},
+			{"4", "5", "6"},
+			{"7", "8", "9"},
+		};
+
+		public int RemainingBlocks = 9;
+		public void DisplayBoard()
+		{
+
+			//TODO: Output the board to the console
+			Console.WriteLine();
+			for (int i = 0; i < GameBoard.GetLength(0); i++)
+			{
+				for (int j = 0; j < GameBoard.GetLength(1); j++)
+				{
+					Console.Write("|" +GameBoard[i, j]+ "|");
+				}
+				Console.WriteLine();
+			}
+
+
+
+		}
+	}
+}
---
+++ ticTac/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace ticTac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartGame();
+        }
+
+        static void StartGame()
+        {
+            // TODO: Setup your game. Create a new method that creates your players and instantiates the game class. Call that method in your Main method.
+            // You are requesting a Winner to be returned, Determine who the winner is output the celebratory message to the correct player. If it's a draw, tell them that there is no winner. 
+            Player player1 = new Player();
+            Player player2 = new Player();
+            Game myGame = new Game(player1, player2);
+            Player winner = myGame.Play();
+            if (winner != null)
+            {
+                Console.WriteLine($"{winner.Name} is the Winner as {winner.Marker}");
+            } else
+            {
+                Console.WriteLine("Draw!!");
+            }
+            
+        }
+    }
+}
